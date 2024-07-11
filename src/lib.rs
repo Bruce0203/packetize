faebcaff715c6809251ed28c87264e4ce7589aad
Bruce0203 <BruceFreedy@gmail.com>
2024-07-11 12:02:18 +0900

    qualify path

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![feature(generic_const_exprs)]
 
-use ::fast_collections::{generic_array::ArrayLength, Cursor};
-
 pub use packetize_derive::*;
 
 pub mod fast_collections;
@@ -9,15 +7,15 @@ pub mod impls;
 
 pub trait Encode<N>
 where
-    N: ArrayLength,
+    N: ::fast_collections::generic_array::ArrayLength,
 {
-    fn encode(&self, write_cursor: &mut Cursor<u8, N>) -> Result<(), ()>;
+    fn encode(&self, write_cursor: &mut ::fast_collections::Cursor<u8, N>) -> Result<(), ()>;
 }
 
 pub trait Decode<N>
 where
     Self: Sized,
-    N: ArrayLength,
+    N: ::fast_collections::generic_array::ArrayLength,
 {
-    fn decode(read_cursor: &mut Cursor<u8, N>) -> Result<Self, ()>;
+    fn decode(read_cursor: &mut ::fast_collections::Cursor<u8, N>) -> Result<Self, ()>;
 }
