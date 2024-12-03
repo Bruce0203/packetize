1514e0780e960a3a55cbb8c9e29503554b639e6f
Bruce0203 <BruceFreedy@gmail.com>
2024-12-03 13:00:37 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-pub use packetize_derive::packet_stream;
+pub use packetize_derive::*;
 
 pub trait Packet<T> {
     fn get_id(&self, state: &T) -> Option<u32>;
